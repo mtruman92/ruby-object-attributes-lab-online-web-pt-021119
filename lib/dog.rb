6e69acc7b21a8f33d8679545a8e6650815621b7f
Mickae Truman <mtruman@nyit.edu>
2019-02-26 13:20:25 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -19,4 +19,20 @@ def breed
   @breed
 end
 
-
+class Person
+  def name=(name)
+    @name = name
+  end
+  
+  def name
+    @name
+  end
+  
+  def job=(job)
+    @job=job
+  end
+  
+  def job
+    @job
+  end
+    
